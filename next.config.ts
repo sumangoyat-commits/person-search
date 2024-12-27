9fc15fa896b28cb3d40d06a8fc469c2c70b7cf57
gocallum <callumbir@gmail.com>
2024-12-28 05:18:18 +1100

    changed the next.config.ts to add the webpack variables'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
+  // Ensure that Next.js can resolve TypeScript and JavaScript files
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
 
 export default nextConfig;
+
