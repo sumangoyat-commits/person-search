841e13ea7ca3eb62bfd4909ba1939035f563ff3f
Suman Goyat <sumangoyat@hotmail.com>
2025-03-14 22:33:35 +1000

    Update project with new changes

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -59,18 +59,11 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
-      }}
       {...props}
     />
   )
 }
+
 Calendar.displayName = "Calendar"
 
 export { Calendar }
